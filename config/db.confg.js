e473d966946f79e346f723b596425db87bbeeef3
Tiago Leite Santos <tiagoleite@acad.fanese.edu.br>
2022-05-20 21:54:24 -0300

    conectado com o banco

--- config/db.confg.js
+++ config/db.confg.js
@@ -5,14 +5,17 @@ const mysql = require('mysql');
 const dbConn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'teste',
+    password: 'root',
     database: 'livraria'
 });
 
 dbConn.connect(function(err){
-
-    if(err) throw err;
-    console.log("Ocorreu um erro ao conectar ao banco de dados");
+    if(err){
+        throw err
+        console.log("Ocorreu um erro ao conectar ao banco de dados");
+    }else{
+    console.log("Conectado com sucesso!");
+    }
 });
 
 module.exports = dbConn;
